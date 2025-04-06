b91b81d3ae052ab53cfe66baaf0bf8b3cb6384a0
4mmang <ammangdeveloper@gmail.com>
2025-04-07 07:07:31 +0800

    laravel vue

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { usePage } from '@inertiajs/vue3'
+
+const { data } = usePage().props
+</script>
+
+<template>
+  <div>
+    <h2>Daftar Mahasiswa:</h2>
+    <ul>
+      <li v-for="(item, index) in data">
+        {{ index + 1 }}.
+        Nama: {{ item.name }} |
+        NIM: {{ item.nim }} |
+        Angkatan: {{ item.angkatan }}
+      </li>
+    </ul>
+  </div>
+</template>
